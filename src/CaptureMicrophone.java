38393aacc7de49072471de0534f88c604cb75a6b
mordes <csongorhegedus2@indamail.hu>
2018-10-10 12:19:09 +0200

    fix microphone issue

--- src/CaptureMicrophone.java
+++ src/CaptureMicrophone.java
@@ -5,40 +5,46 @@ import java.io.IOException;
 
 public class CaptureMicrophone {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("sound test");
-        try{
-            AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 1, 4, 44100,false);
+        try {
+            AudioFormat format = new AudioFormat(8000.0F, 16, 1, true, false);
 
             DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-            if(!AudioSystem.isLineSupported(info))
+            if (!AudioSystem.isLineSupported(info))
                 System.out.println("line not supported");
 
-            TargetDataLine targetLine = (TargetDataLine)AudioSystem.getLine(info);
+            TargetDataLine targetLine = (TargetDataLine) AudioSystem.getLine(info);
             targetLine.open(); //start recording
 
             System.out.println("start recording");
             targetLine.start();
 
-            Thread thread = new Thread(){
-                @Override public void run(){
-                    AudioInputStream audioInputStream = new AudioInputStream(targetLine);
-                    File audioFile = new File("record.wav");
-                    try {
-                        AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, audioFile);
-                    }catch (IOException ex){
-                        ex.printStackTrace();
+            try {
+                Thread thread = new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            AudioInputStream audioInputStream = new AudioInputStream(targetLine);
+                            File audioFile = new File("record.wav");
+                            audioFile.createNewFile();
+                            AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, audioFile);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                        System.out.println("stopped");
                     }
-                    System.out.println("stopped");
-                }
-            };
+                };
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
             Thread.sleep(5000);
             targetLine.stop();
             targetLine.close();
             System.out.println("end");
-        }catch (LineUnavailableException ex){
+        } catch (LineUnavailableException ex) {
             ex.printStackTrace();
-        }catch (InterruptedException ex){
+        } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
     }
